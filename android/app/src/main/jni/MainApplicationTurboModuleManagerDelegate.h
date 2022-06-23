754c1f4d791999c67c06551c2a9d544ff1372e5a
TuanLamAnh <lamanhtuan1603@gmail.com>
2022-06-23 16:10:09 +0700

    4:09 23/06/2022

--- android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
+++ android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
@@ -14,7 +14,7 @@ class MainApplicationTurboModuleManagerDelegate
  public:
   // Adapt it to the package you used for your Java class.
   static constexpr auto kJavaDescriptor =
-      "Lcom/AwesomeProject/App/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
+      "Lcom/bmd/demoabh/tuan//newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
 
   static jni::local_ref<jhybriddata> initHybrid(jni::alias_ref<jhybridobject>);
 
--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/AwesomeProject/App/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/bmd/demoabh/tuan/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
